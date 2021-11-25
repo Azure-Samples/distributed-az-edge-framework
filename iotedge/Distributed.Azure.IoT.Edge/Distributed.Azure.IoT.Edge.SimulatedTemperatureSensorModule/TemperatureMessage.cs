734045d619ee724fab752b93c687197ede34c7a8
Suneet Nangia <suneetnangia@gmail.com>
2021-11-25 17:34:13 +0000

    Cleaned Helm Charts Branch

--- iotedge/Distributed.Azure.IoT.Edge/Distributed.Azure.IoT.Edge.SimulatedTemperatureSensorModule/TemperatureMessage.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Distributed.Azure.IoT.Edge.SimulatedTemperatureSensorModule
-{
-    internal record TemperatureMessage(TemperaturePressure Machine, TemperaturePressure Ambient);
-
-    internal record TemperaturePressure(double Temperature, double Pressure);
-}
