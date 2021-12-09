507ae1c7a6e9d791b543b9e25400c025cef0e417
simonjaeger <simon.jager@live.com>
2021-12-09 14:52:55 +0100

    feat: add opc publisher to upgrade

--- deployment/build-and-deploy-images.ps1
+++ deployment/build-and-deploy-images.ps1
@@ -41,13 +41,30 @@ az acr build --image datagatewaymodule:$deploymentId --registry $acrName --file 
 az acr build --image simulatedtemperaturesensormodule:$deploymentId --registry $acrName --file Distributed.IoT.Edge.SimulatedTemperatureSensorModule/Dockerfile .
 Set-Location -Path $deplymentDir
 
+# ----- Build and Push Containers (OPC Publisher)
+Write-Title("Build and Push Containers (OPC Publisher)")
+if (!(Test-Path .\..\..\Industrial-IoT-Temp))
+{
+    git clone -b feature/dapr-adapter https://github.com/simonjaeger/Industrial-IoT .\..\..\Industrial-IoT-Temp
+}
+Set-Location -Path .\..\..\Industrial-IoT-Temp
+git pull
+$Env:BUILD_SOURCEBRANCH = "feature/dapr-adapter"
+$Env:Version_Prefix = $deploymentId
+.\tools\scripts\acr-build.ps1 -Path .\modules\src\Microsoft.Azure.IIoT.Modules.OpcUa.Publisher\src -Registry $acrName
+Set-Location -Path $deplymentDir
+
 # ----- Run Helm
 Write-Title("Upgrade Pod/Containers with Helm in Cluster")
 $datagatewaymoduleimage = $acrName + ".azurecr.io/datagatewaymodule:" + $deploymentId
 $simtempimage = $acrName + ".azurecr.io/simulatedtemperaturesensormodule:" + $deploymentId
+$opcplcimage = "mcr.microsoft.com/iotedge/opc-plc:2.2.0"
+$opcpublisherimage = $acrName + ".azurecr.io/dapr-adapter/iotedge/opc-publisher:" + $deploymentId
 helm upgrade iot-edge-accelerator ./helm/iot-edge-accelerator `
     --set-string images.datagatewaymodule="$datagatewaymoduleimage" `
     --set-string images.simulatedtemperaturesensormodule="$simtempimage" `
+    --set-string images.opcplcmodule="$opcplcimage" `
+    --set-string images.opcpublishermodule="$opcpublisherimage" `
 
 $runningTime = New-TimeSpan -Start $startTime
 
