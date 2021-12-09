7d93d784ea7e764eb6bb1ef4e336031f85cb2150
Dag König <dagk@microsoft.com>
2021-12-09 11:43:09 +0100

    build and update images in k8s

--- deployment/deploy-aks-dapr.ps1
+++ deployment/deploy-aks-dapr.ps1
@@ -91,5 +91,7 @@ if($DeleteResourceGroup)
     }
 }
 
+$env:RESOURCEGROUPNAME=$resourceGroupName
+
 $runningTime = New-TimeSpan -Start $startTime
 Write-Host "Running time:" $runningTime.ToString() -ForegroundColor Yellow
