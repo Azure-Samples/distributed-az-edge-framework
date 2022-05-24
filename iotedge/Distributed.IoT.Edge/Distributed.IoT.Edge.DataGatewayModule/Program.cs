a3de645ee17608aa157991d226b3e403aac5075e
Suneet Nangia <suneetnangia@gmail.com>
2022-05-24 13:17:26 +0100

    Fixed end of file crlf

--- iotedge/Distributed.IoT.Edge/Distributed.IoT.Edge.DataGatewayModule/Program.cs
+++ iotedge/Distributed.IoT.Edge/Distributed.IoT.Edge.DataGatewayModule/Program.cs
@@ -48,4 +48,3 @@ app.MapGrpcService<SubscriptionService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
-
