f0a70eb35cbdcddb5601aa9b0b08c3355a9fbcae
Suneet Nangia <suneetnangia@gmail.com>
2022-05-24 13:00:15 +0100

    Triggering build

--- iotedge/Distributed.IoT.Edge/Distributed.IoT.Edge.DataGatewayModule/Program.cs
+++ iotedge/Distributed.IoT.Edge/Distributed.IoT.Edge.DataGatewayModule/Program.cs
@@ -48,3 +48,4 @@ app.MapGrpcService<SubscriptionService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
+
