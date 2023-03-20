3da6e0546ca1dc85cf41252c4fcd3e45efa6df69
Katrien De Graeve <katriendg@users.noreply.github.com>
2023-03-20 13:29:27 +0100

    nested data small fixes

--- deployment/deploy-az-dev-bootstrapper.ps1
+++ deployment/deploy-az-dev-bootstrapper.ps1
@@ -13,7 +13,7 @@ Param(
 )
 
 # Import text utilities module.
-Import-Module -Name .\modules\text-utils.psm1
+Import-Module -Name ./modules/text-utils.psm1
 
 Write-Title("Start Deploying")
 $startTime = Get-Date
