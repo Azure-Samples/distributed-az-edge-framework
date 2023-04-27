f944ee893741f6b2a37c868079bb2966959dfd0c
Marcel Bindseil <marcelbindseil@gmail.com>
2023-04-27 19:32:52 +0200

    WIP workflowEngine

--- iotedge/Distributed.IoT.Edge/Distributed.IoT.Edge.WorkflowModule/Workflows/EnrichTelemetryWorkflow.cs
+++ iotedge/Distributed.IoT.Edge/Distributed.IoT.Edge.WorkflowModule/Workflows/EnrichTelemetryWorkflow.cs
@@ -1,11 +1,23 @@
 namespace Distributed.IoT.Edge.WorkflowModule.Workflows;
 
 using Dapr.Workflow;
+using WorkflowConsoleApp.Activities;
 
 public class EnrichTelemetryWorkflow : Workflow<string, bool>
 {
-    public override Task<bool> RunAsync(WorkflowContext context, string input)
+    public override async Task<bool> RunAsync(WorkflowContext? context, string input)
     {
-        throw new NotImplementedException();
+        if (context == null)
+        {
+            return false;
+        }
+
+        var enrichedData = await context.CallActivityAsync<string>(nameof(EnrichmentActivity), input);
+        if (string.IsNullOrEmpty(enrichedData))
+        {
+            return false;
+        }
+
+        return await context.CallActivityAsync<bool>(nameof(PublishActivity), enrichedData);
     }
 }
